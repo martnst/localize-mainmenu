6ef20621a6ac2bc7816d2f4657e989a2fce631a1
Martin Stemmle <marste@msmix.de>
2014-12-21 12:31:30 +0100

    Added script to generate a commands to search for translations within the installed apps

---
+++ generate_search_translations_commands.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var colors = require('colors');
+var template_file_name = '_template.json';
+var template_keys = require('./languages/' + template_file_name);
+
+
+fs.readdir('./languages', function(err, files) {
+  if (err) console.log(err);
+  files.forEach(function(fileName) {
+    if (fileName == template_file_name) { return; }
+    var language = fileName.substr(0, fileName.length-5);
+    console.log("\n" + language + ":\n");
+
+    var search_localized_strings_cmd = "cd /Applications && cat *.app/Contents/Resources/" + language + ".lproj/MainMenu.strings | grep '";
+
+    for (var key in template_keys) {
+      search_localized_strings_cmd += '\\|"' + key + '"';
+    }
+
+    search_localized_strings_cmd += '\' -A 1';
+
+    console.log(search_localized_strings_cmd);
+    console.log("\n\n");
+  });
+}); 
